727e0bca50ba80d93491c02c8cdd47979a451831
Vera <75651598+Verass915@users.noreply.github.com>
2021-11-16 14:27:59 +0400

    Add files via upload

---
+++ precache-manifest.826513d03d7e8a3b36b2464d29b96125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795b47eb318869d80a12dc94f7bc0dc3",
+    "url": "/sortirovka/index.html"
+  },
+  {
+    "revision": "b4a53fa4b5380b63c180",
+    "url": "/sortirovka/static/js/2.fc95608b.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/sortirovka/static/js/2.fc95608b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07486be411a4205aa54a",
+    "url": "/sortirovka/static/js/main.f2b362da.chunk.js"
+  },
+  {
+    "revision": "2aaf630b0237795703ef",
+    "url": "/sortirovka/static/js/runtime-main.a67ca9a5.js"
+  }
+]);
